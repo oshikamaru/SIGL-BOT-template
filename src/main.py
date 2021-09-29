3bc3937046c1cd6dbd8b379c0e93a9ca1eb98f55
Alexis Nguyen <alexis.nguyen@epita.fr>
2021-09-29 20:47:33 +0200

    Add count function to bot

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 import os
+import discord
 from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.members = True
 
 bot = commands.Bot(
     command_prefix="!",  # Change to desired prefix
-    case_insensitive=True  # Commands aren't case-sensitive
+    case_insensitive=True,  # Commands aren't case-sensitive
+    intents=intents
 )
 
 bot.author_id = 336443857103486976  # Change to your discord id!!!
@@ -17,11 +22,24 @@ async def on_ready():  # When the bot is ready
 async def pong(ctx):
     await ctx.send('pong')
 
+
 # Function that write back the name of the user typing the command !name #
-@bot.event
-async def on_message(message):
-    if (message.content == "!name"):
-        await message.channel.send(message.author)
+@bot.command(name="name")
+async def name(ctx):
+    await ctx.send(ctx.author)
+
+
+# Function that count the number of members on the server typing the command !count #
+@bot.command(name="count")
+async def count(ctx):
+    members = bot.guilds[0].members
+    size = len(members) - 1
+    # text = ' members are online' 
+    await ctx.send(size)
+    for member in members :
+        if (not member.bot):
+            await ctx.send(member.name)
+
 
 token = "ODkyODIyMTY4MzE0OTk4ODA0.YVSfcA.ixPqpvPRJCQPx6k-lfoDiFkvnpo"
 bot.run(token)  # Starts the bot
